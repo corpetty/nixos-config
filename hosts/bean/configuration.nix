b234f64b438e9d81320b2fd6179ea8c2ba0b35da
Corey Petty <corey@status.im>
2023-10-10 14:02:45 -0400

    setup base.nix role and main host.

---
+++ hosts/bean/configuration.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../roles/base.nix
+    ../../roles/zfs.nix
+    ../../roles/users.nix
+    ../../roles/network.nix
+    ../../roles/locate.nix
+    ../../roles/autofs.nix
+    ../../roles/desktop.nix
+    ../../roles/bluetooth.nix
+    ../../roles/physlock.nix
+    ../../roles/printer.nix
+    ../../roles/gaming.nix
+    ../../roles/docker.nix
+    ../../roles/music.nix
+    ../../roles/zerotier.nix
+    ../../roles/yubikey.nix
+    ../../roles/syncthing.nix
+    ../../roles/restic.nix
+    ../../roles/work.nix
+    ../../roles/docs.nix
+    ../../roles/landing.nix
+    ../../roles/optical.nix
+    ../../roles/dnsmasq.nix
+    ../../roles/qemu.nix
+    ../../roles/torrent.nix
+    ../../roles/builder.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.grub.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
+
+  # Resume
+  systemd.sleep.extraConfig = "HibernateMode=reboot";
+
+  # Sensors
+  boot.kernelModules = [ "nct6775" "coretemp" "i2c-1" ];
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
+
+  networking = {
+    hostName = "bean";
+    hostId = "9fbd8b5d";
+    useDHCP = false;
+    interfaces = {
+      eno1 = { useDHCP = true; };
+    };
+  };
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "pl";
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+  # Avoid difference when dual-booting Windows.
+  time.hardwareClockInLocalTime = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  # Video
+  services.xserver.videoDrivers = [ "nvidia" "intel" ];
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    nvidiaSettings = true;
+    modesetting.enable = true;
+  };
+
+  # Power Management
+  powerManagement.cpuFreqGovernor = "performance";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "20.09";
+
+  systemd.coredump.enable = true;
+}
